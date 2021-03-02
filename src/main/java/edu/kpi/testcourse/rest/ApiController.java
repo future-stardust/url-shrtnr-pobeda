3689699b8aea33c9c1fe87435487d8035699d9e2
MazanYan <mazanyan027@gmail.com>
2021-03-02 20:50:46 +0200

    Remove authorization to test other routes

--- src/main/java/edu/kpi/testcourse/rest/ApiController.java
+++ src/main/java/edu/kpi/testcourse/rest/ApiController.java
@@ -4,13 +4,10 @@ import edu.kpi.testcourse.Main;
 import io.micronaut.http.MediaType;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
-import io.micronaut.security.annotation.Secured;
-import io.micronaut.security.rules.SecurityRule;
 
 /**
  * REST API controller that provides logic for Micronaut framework.
  */
-@Secured(SecurityRule.IS_AUTHENTICATED)
 @Controller
 public class ApiController {
 
