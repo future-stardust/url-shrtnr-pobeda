549b38fd218b4afcbc7db8e81f5aabc86b5237c4
Vadim Dudka <niqz99@gmail.com>
2021-03-03 15:47:50 +0200

    Fix encoding error

--- src/main/java/edu/kpi/testcourse/bigtable/BigTable.java
+++ src/main/java/edu/kpi/testcourse/bigtable/BigTable.java
@@ -1,7 +1,7 @@
 package edu.kpi.testcourse.bigtable;
 
-// ⚠️ Please, pay attention, that you should not use any 3rd party persistence library (e.g. data
-// ⚠️ base, implementation of key-value storage, etc)
+// Please, pay attention, that you should not use any 3rd party persistence library (e.g. data
+// base, implementation of key-value storage, etc)
 
 /**
  * BigTable is a key-value storage...
