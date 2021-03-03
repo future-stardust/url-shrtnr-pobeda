f07e8f28c9c81540bd045ee92acf7ec6800ab6cb
Vadim Dudka <niqz99@gmail.com>
2021-03-03 23:43:58 +0200

    Add User dto

--- src/main/java/edu/kpi/testcourse/dto/User.java
+++ src/main/java/edu/kpi/testcourse/dto/User.java
@@ -2,4 +2,20 @@ package edu.kpi.testcourse.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-public record User(@JsonProperty("email") String email, @JsonProperty("password") String password) {}
+/**
+ * Dto for User.
+ */
+public record User(@JsonProperty("email") String email, @JsonProperty("password") String password) {
+
+  /**
+   * Check if user data is valid.
+   *
+   * @return true if data is valid, otherwise false
+   */
+  public boolean isValid() {
+    return
+      email != null && !email.isEmpty()
+        && password != null && !password.isEmpty();
+  }
+
+}
