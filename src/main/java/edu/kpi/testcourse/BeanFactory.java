885af8303659323b67f045bae088c6d88a5d134d
Ringa | Virda <ringanaro@gmail.com>
2021-03-15 21:34:47 +0200

    Update bean factory to produce different big tables for testing and prod

--- src/main/java/edu/kpi/testcourse/BeanFactory.java
+++ src/main/java/edu/kpi/testcourse/BeanFactory.java
@@ -4,19 +4,29 @@ import edu.kpi.testcourse.bigtable.BigTable;
 import edu.kpi.testcourse.bigtable.BigTableConf;
 import edu.kpi.testcourse.bigtable.BigTableImpl;
 import io.micronaut.context.annotation.Factory;
+import io.micronaut.context.annotation.Primary;
+import io.micronaut.context.annotation.Requires;
+import java.io.IOException;
+import java.nio.file.Files;
 import javax.inject.Singleton;
 
+/**
+ * Factory for something.
+ */
 @Factory
 public class BeanFactory {
 
-  private final String PATH_TO_STORAGE_WINDOWS =
-    System.getenv("appdata") + "\\url-shrtnr-pobeda";
-  private final String PATH_TO_STORAGE_LINUX = "var/lib/url-shrtnr-pobeda";
+  private static final String PATH_TO_STORAGE_WINDOWS =
+      System.getenv("appdata") + "\\url-shrtnr-pobeda";
+  private static final String PATH_TO_STORAGE_LINUX = "/var/lib/url-shrtnr-pobeda";
 
+
+  @Requires(notEnv = "test")
+  @Primary
   @Singleton
   BigTable createBigTable() {
     // Check OS and select correct configuration
-    String osName = System.getProperty("os.name");
+    String osName = System.getProperty("os.name").toLowerCase();
     BigTableConf conf;
     if ((osName.contains("win"))) {
       conf = new BigTableConf(PATH_TO_STORAGE_WINDOWS);
@@ -27,4 +37,13 @@ public class BeanFactory {
     return new BigTableImpl(conf);
   }
 
+  @Singleton
+  BigTable createTestBigTable() {
+    try {
+      return new BigTableImpl(
+          new BigTableConf(Files.createTempDirectory("url-shrtnr-pobeda-tests")));
+    } catch (IOException exception) {
+      throw new RuntimeException(exception);
+    }
+  }
 }
