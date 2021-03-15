18e0896bbf1791654781124b60846623f34fb2f3
Ringa | Virda <ringanaro@gmail.com>
2021-03-15 22:39:42 +0200

    Records, here we go again

--- src/main/java/edu/kpi/testcourse/dto/ShortLink.java
+++ src/main/java/edu/kpi/testcourse/dto/ShortLink.java
@@ -1,5 +1,6 @@
 package edu.kpi.testcourse.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import io.micronaut.core.annotation.Introspected;
 import java.net.URL;
 
@@ -7,48 +8,8 @@ import java.net.URL;
  * Entity for short link record in the data storage.
  */
 @Introspected
-public final class ShortLink {
-  private String alias;
-  private String email;
-  private String url;
+public record ShortLink(@JsonProperty("alias") String alias,
+                        @JsonProperty("email") String email,
+                        @JsonProperty("url") String url) {
 
-  /**
-   * Default constructor.
-   *
-   * @param alias alias for a "long" link
-   * @param email user's email
-   * @param url link to shorten
-   */
-  public ShortLink(String alias, String email, String url) {
-    this.alias = alias;
-    this.email = email;
-    this.url = url;
-  }
-
-  /**
-   * Getter for alias.
-   *
-   * @return shortLink
-   */
-  public String alias() {
-    return alias;
-  }
-
-  /**
-   * Getter for user email.
-   *
-   * @return email
-   */
-  public String email() {
-    return email;
-  }
-
-  /**
-   * Getter for destination.
-   *
-   * @return destination
-   */
-  public String url() {
-    return url;
-  }
 }
